a642b32becbf98a9d8c536d9e2fee44952128e2f
Eric Weinstein <eric.q.weinstein@gmail.com>
2020-05-23 19:57:15 -0700

    Complete support for function evaluation (including closures).

--- object/environment.go
+++ object/environment.go
@@ -2,15 +2,30 @@ package object
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
+}
+
+// In order to support closures and lexical scoping, we need to
+// ensure we extend all outer environments with new bindings
+// (rather than overwriting, since an `x` inside a function body
+// shouldn't overwrite an `x` already bound in an outer scope).
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
 }
 
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
+
 	return obj, ok
 }
 
